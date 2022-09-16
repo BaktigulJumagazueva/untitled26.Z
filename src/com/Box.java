a890a1f3373cadfef466f31fdce9730693ba30a0
13 <baktyguljumagazyeva@gmail.com>
2022-09-16 21:11:47 +0600

    DZ26

--- src/com/Box.java
+++ src/com/Box.java
@@ -1,2 +1,20 @@
-package com;public class Box {
+package com;
+
+public class Box<T> {
+    private T box;
+
+    public Box(T box) {
+        this.box = box;
+    }
+
+    public T getBox() {
+        return box;
+    }
+
+    public void setBox(T box) {
+        this.box = box;
+    }
+    public static <box> void generic(box T){
+
+    }
 }
--- src/com/Main.java
+++ src/com/Main.java
@@ -1,11 +1,9 @@
+package com;
+
 public class Main {
     public static void main(String[] args) {
+Box box= new Box<>(223);
+        System.out.println(box.getBox());
 
-
-    }
-
-
-    public static <T> void Box(T[] args) {
-        System.out.println("T " + args[4]);
     }
 }
